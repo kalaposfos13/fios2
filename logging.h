ae88a2ede6a0af34b9ee2d0b4d43cfa0f006100f
kalaposfos13 <153381648+kalaposfos13@users.noreply.github.com>
2025-06-09 12:44:00 +0200

    Silence debug logs

--- logging.h
+++ logging.h
@@ -17,7 +17,7 @@ void PrintLog(const char* log_level, unsigned int line_num, const char* function
     fmt::print("fios2.cpp:{} <{}> {}: {}\n", line_num, log_level, function, message);
 }
 
-#define LOG_DEBUG(...)    PrintLog("Debug",    __LINE__, __func__, __VA_ARGS__)
+#define LOG_DEBUG(...)    // PrintLog("Debug",    __LINE__, __func__, __VA_ARGS__)
 #define LOG_INFO(...)     PrintLog("Info",     __LINE__, __func__, __VA_ARGS__)
 #define LOG_WARNING(...)  PrintLog("Warning",  __LINE__, __func__, __VA_ARGS__)
 #define LOG_ERROR(...)    PrintLog("Error",    __LINE__, __func__, __VA_ARGS__)
